6517e0971578d6d12861b011297f3d4172aad54d
DanielLieskovsky <danol147@gmail.com>
2022-10-20 10:28:22 +0200

    Repairing previous commit

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,17 +7,6 @@ import { Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  HEROES: Hero[] = [
-    { id: 12, name:"Dr. Nice"},
-    { id: 13, name: "Bombasto"},
-    { id: 14, name: "Celeritas"},
-    { id: 15, name: "Magneta"},
-    { id: 16, name: "RubberMan"},
-    { id: 17, name: "Dynama"},
-    { id: 18, name: "Dr. IQ"},
-    { id: 19, name: "Magma"},
-    { id: 20, name: "Tornado"}
-  ];
 
   constructor() { }
 
