5960f431186eea5f52d9b51c0db003f253362c35
DanielLieskovsky <danol147@gmail.com>
2022-10-26 16:18:38 +0200

    Generate service chapter done

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from "./mock-heroes";
+import { Hero} from "./hero";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+}
+
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +8,19 @@ import {Hero} from "../hero";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- heroes = HEROES;
+ heroes: Hero[] = [];
  selectedHero?: Hero;
 
  onSelect(hero: Hero) {
    this.selectedHero = hero;
  }
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+   this.getHeroes();
   }
 
+  getHeroes() : void {
+   this.heroes = this.heroService.getHeroes();
+  }
 }
