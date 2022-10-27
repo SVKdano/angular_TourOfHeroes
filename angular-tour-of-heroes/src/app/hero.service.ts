1b732c93f5a0000e1679501c8d1ed145f948a5ce
DanielLieskovsky <danol147@gmail.com>
2022-10-27 10:28:12 +0200

    Observable data chapter done

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from "rxjs";
 import { HEROES } from "./mock-heroes";
 import { Hero} from "./hero";
 
@@ -9,8 +10,9 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -21,6 +21,6 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes() : void {
-   this.heroes = this.heroService.getHeroes();
+   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes );
   }
 }
