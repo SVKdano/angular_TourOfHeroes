e447e9ef1199d96b1bc7f066652938e1318b0e20
DanielLieskovsky <danol147@gmail.com>
2022-11-09 16:17:24 +0100

    routing chapter done

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -20,6 +20,7 @@ export class DashboardComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1,5))
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1,5))
   }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -12,21 +12,15 @@ import { MessagesService } from "../services/messages.service";
 
 export class HeroesComponent implements OnInit {
  heroes: Hero[] = [];
- selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessagesService) { }
-
-  onSelect(hero: Hero) {
-   this.selectedHero = hero;
-   this.messageService.add(`HeroesComponent: Selected hero id = ${ hero.id }`);
-  }
-
 
   ngOnInit(): void {
    this.getHeroes();
   }
 
   getHeroes() : void {
-   this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes );
+   this.heroService.getHeroes()
+     .subscribe(heroes => this.heroes = heroes );
   }
 }
